958e0e1506327e42e00034004504ccb932fdcea9
Martin Erren <martin3r@Mac.fritz.box>
2026-01-11 19:47:56 +0100

    tools

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,10 @@ use Platform\Hcm\Http\Controllers\Api\EmployeeNostradamusController;
 Route::get('/employees/nostradamus', [EmployeeNostradamusController::class, 'index']);
 Route::get('/employees/nostradamus/health', [EmployeeNostradamusController::class, 'health']);
 
+/**
+ * Bewegungsdaten-Endpunkt für Nostradamus
+ * 
+ * Empfängt Bewegungsdaten (Stempelzeiten, Urlaubstage, Abwesenheitstage)
+ * und speichert sie in den HCM-Tabellen.
+ */
+Route::post('/movements', [\Platform\Hcm\Http\Controllers\Api\MovementDataController::class, 'store']);
